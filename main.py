4da96484454dc2b8d31ffa65b89a0939975e378f
SnehalChodankar <snehalchodankar95@gmail.com>
2024-04-27 20:47:45 +0530

    data file upgade

--- main.py
+++ main.py
@@ -5,23 +5,11 @@ import requests
 import ui
 
 question_bank = []
-
-
-def get_quiz_data():
-    response = requests.get(url="https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean")
-    response.raise_for_status()
-
-    data = response.json()
-    # print(data)
-
-    for question in data["results"]:
-        question_text = question["question"]
-        question_answer = question["correct_answer"]
-        new_question = Question(question_text, question_answer)
-        question_bank.append(new_question)
-
-
-get_quiz_data()
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
 quiz_ui = ui.QuizInterface(quiz)
